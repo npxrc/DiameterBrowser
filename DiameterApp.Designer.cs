b25fb1bcb300ad6539bcd78fcf4111ef3558502d
npxrc <npxrc@duck.com>
2024-04-25 20:35:55 -0500

    Add project files.

---
+++ DiameterApp.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace DiameterBrowser
+{
+    partial class DiameterApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DiameterApp));
+            this.webbrowser = new CefSharp.WinForms.ChromiumWebBrowser();
+            this.url = new System.Windows.Forms.TextBox();
+            this.navigate = new System.Windows.Forms.Button();
+            this.back = new System.Windows.Forms.Button();
+            this.forward = new System.Windows.Forms.Button();
+            this.refresh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // webbrowser
+            // 
+            this.webbrowser.ActivateBrowserOnCreation = false;
+            this.webbrowser.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.webbrowser.Location = new System.Drawing.Point(91, 44);
+            this.webbrowser.Name = "webbrowser";
+            this.webbrowser.Size = new System.Drawing.Size(727, 444);
+            this.webbrowser.TabIndex = 0;
+            // 
+            // url
+            // 
+            this.url.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.url.Location = new System.Drawing.Point(637, 1);
+            this.url.Name = "url";
+            this.url.Size = new System.Drawing.Size(189, 22);
+            this.url.TabIndex = 1;
+            this.url.Text = "enter a url";
+            // 
+            // navigate
+            // 
+            this.navigate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.navigate.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.navigate.Location = new System.Drawing.Point(832, 1);
+            this.navigate.Name = "navigate";
+            this.navigate.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.navigate.Size = new System.Drawing.Size(75, 23);
+            this.navigate.TabIndex = 2;
+            this.navigate.Text = "go";
+            this.navigate.UseVisualStyleBackColor = true;
+            // 
+            // back
+            // 
+            this.back.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.back.Location = new System.Drawing.Point(0, 0);
+            this.back.Name = "back";
+            this.back.Size = new System.Drawing.Size(20, 20);
+            this.back.TabIndex = 3;
+            this.back.Text = "<";
+            this.back.UseVisualStyleBackColor = true;
+            // 
+            // forward
+            // 
+            this.forward.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.forward.Location = new System.Drawing.Point(20, 0);
+            this.forward.Name = "forward";
+            this.forward.Size = new System.Drawing.Size(20, 20);
+            this.forward.TabIndex = 4;
+            this.forward.Text = ">";
+            this.forward.UseVisualStyleBackColor = true;
+            // 
+            // refresh
+            // 
+            this.refresh.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.refresh.Location = new System.Drawing.Point(40, 0);
+            this.refresh.Margin = new System.Windows.Forms.Padding(0);
+            this.refresh.Name = "refresh";
+            this.refresh.Size = new System.Drawing.Size(75, 20);
+            this.refresh.TabIndex = 5;
+            this.refresh.Text = "Refresh";
+            this.refresh.UseVisualStyleBackColor = true;
+            // 
+            // DiameterApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(909, 485);
+            this.Controls.Add(this.refresh);
+            this.Controls.Add(this.forward);
+            this.Controls.Add(this.back);
+            this.Controls.Add(this.navigate);
+            this.Controls.Add(this.url);
+            this.Controls.Add(this.webbrowser);
+            this.Font = new System.Drawing.Font("Noto Sans", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MinimumSize = new System.Drawing.Size(320, 420);
+            this.Name = "DiameterApp";
+            this.Text = "Diameter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CefSharp.WinForms.ChromiumWebBrowser webbrowser;
+        private System.Windows.Forms.TextBox url;
+        private System.Windows.Forms.Button navigate;
+        private System.Windows.Forms.Button back;
+        private System.Windows.Forms.Button forward;
+        private System.Windows.Forms.Button refresh;
+    }
+}
+
---
+++ DiameterApp.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Runtime.Remoting.Channels;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace DiameterBrowser
+{
+    public partial class DiameterApp : Form
+    {
+        private string appDataFolder = "Diameter";
+        private string baseUrl = "https://www.perplexity.ai/?q=";
+        private bool control = false;
+
+        public DiameterApp()
+        {
+            InitializeComponent();
+            OnResize();
+            try
+            {
+                baseUrl = ReadFromFile("baseurl");
+            } catch (Exception ex) { 
+                Console.Error.WriteLine(ex.Message);
+                WriteToFile("baseurl", "https://perplexity.ai/?q=");
+                Application.Exit();
+            }
+            InitializeCefSharp();
+            this.Resize += (sender, e) => OnResize();
+            navigate.Click += (sender, e) => Navigate();
+            back.Click += (sender, e)=> webbrowser.Back();
+            forward.Click += (sender, e) => webbrowser.Forward();
+            refresh.Click += (sender, e) => webbrowser.Reload();
+            // Assuming this code is in the constructor or initialization method of your form
+            this.KeyDown += (sender, e) => KeyDownEvent(sender, e);
+            this.KeyUp += (sender, e) => KeyUpEvent(sender, e);
+
+            url.KeyDown += (sender, e) => {
+                if (e.KeyCode == Keys.Enter)
+                {
+                    Navigate();
+                    webbrowser.Focus();
+                }
+            };
+        }
+
+        private void KeyDownEvent(object sender, KeyEventArgs e)
+        {
+            {
+                if (e.KeyCode == Keys.ControlKey)
+                {
+                    MessageBox.Show("AHHHHHHHHH");
+                    control = true;
+                }
+                else if (control)
+                {
+                    if (e.KeyCode == Keys.W)
+                    {
+                        Application.Exit();
+                    }
+                    else if (e.KeyCode == Keys.E)
+                    {
+                        url.Focus();
+                    }
+                    else if (e.KeyCode == Keys.R)
+                    {
+                        webbrowser.Reload();
+                    }
+                }
+            };
+        }
+        private void KeyUpEvent(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.ControlKey)
+            {
+                control = false;
+            }
+        }
+
+        private void InitializeCefSharp()
+        {
+            var settings = new CefSettings();
+            Cef.Initialize(settings);
+
+            webbrowser.Load("https://google.com");
+
+            //webbrowser.BringToFront();
+        }
+
+        private void OnResize()
+        {
+            if (this.ClientSize.Width <= Screen.PrimaryScreen.WorkingArea.Width/2)
+            {
+                this.Text = "Radius";
+            }
+            else
+            {
+                this.Text = "Diameter";
+            }
+            webbrowser.Height = (this.ClientSize.Height-25);
+            webbrowser.Width = this.ClientSize.Width;
+
+            webbrowser.Anchor = AnchorStyles.Bottom;
+
+            webbrowser.Location = new Point(
+                this.ClientSize.Width - webbrowser.Width,//align horizontally
+                this.ClientSize.Height - webbrowser.Height
+            );
+        }
+
+        private void Navigate()
+        {
+            if (url.Text.Length > 0)
+            {
+                if (url.Text.ToLower().Trim() == "google")
+                {
+                    MessageBox.Show("Search engine switched to Google.", "Search Engine", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    baseUrl = "https://google.com/search?q=";
+                    WriteToFile("baseurl", baseUrl);
+                }
+                else if (url.Text.ToLower().Trim() == "bing")
+                {
+                    MessageBox.Show("Search engine switched to Bing.", "Search Engine", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    baseUrl = "https://bing.com/search?q=";
+                    WriteToFile("baseurl", baseUrl);
+                }
+                else if (url.Text.ToLower().Trim() == "perplexity" || url.Text.ToLower().Trim() == "perplexity ai" || url.Text.ToLower().Trim() == "perplexity.ai")
+                {
+                    MessageBox.Show("Search engine switched to Perplexity.", "Search Engine", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    baseUrl = "https://www.perplexity.ai/?q=";
+                    WriteToFile("baseurl", baseUrl);
+                }
+                else if (url.Text.StartsWith("http:") || url.Text.StartsWith("https:") || url.Text.StartsWith("www."))
+                {
+                    webbrowser.Load(url.Text);
+                }
+                else if (url.Text.Split('.')[url.Text.Split('.').Length - 1].Length > 1)
+                {
+                    webbrowser.Load(url.Text);
+                }
+                else
+                {
+                    string toLoad = baseUrl + url.Text;
+                    webbrowser.Load(toLoad);
+                }
+            }
+        }
+        private string ReadFromFile(string filename)
+        {
+            try
+            {
+                string localAppDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+                string gameFolderPath = Path.Combine(localAppDataPath, appDataFolder);
+
+                string cookiesFilePath = Path.Combine(gameFolderPath, filename);
+
+                if (File.Exists(cookiesFilePath))
+                {
+                    string textFromFile = File.ReadAllText(cookiesFilePath);
+                    return textFromFile;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+        private void WriteToFile(string filePath, string toWrite)
+        {
+            try
+            {
+                string localAppDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+                string gameFolderPath = Path.Combine(localAppDataPath, appDataFolder);
+
+                if (!Directory.Exists(gameFolderPath))
+                {
+                    Directory.CreateDirectory(gameFolderPath);
+                }
+
+                string cookiesFilePath = Path.Combine(gameFolderPath, filePath);
+
+                string textToWrite = toWrite;
+                File.WriteAllText(cookiesFilePath, textToWrite);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
